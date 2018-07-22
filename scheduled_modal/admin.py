34da0ebfeec13f6c5261ee5a3efb2b5720e3b1cd
Alban Tiberghien <alban.tiberghien@gmail.com>
2018-07-22 14:18:09 +0200

    Customize admin

--- scheduled_modal/admin.py
+++ scheduled_modal/admin.py
@@ -3,7 +3,8 @@ from .models import Announcement
 
 
 class AnnouncementAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'begin_date', 'end_date', 'show_period')
+    list_editable = ('begin_date', 'end_date', 'show_period')
 
 
 admin.site.register(Announcement, AnnouncementAdmin)
